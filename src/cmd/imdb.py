608f27905976a2556db76fcd33dc790b77744739
pipi <njk4401@rit.edu>
2025-10-13 18:16:08 -0400

    pick movie command

--- src/cmd/imdb.py
+++ src/cmd/imdb.py
@@ -3,7 +3,7 @@ import json
 from random import randint
 
 import pandas as pd
-from nextcord import File, Interaction, SlashOption, slash_command
+from nextcord import Embed, File, Interaction, SlashOption, slash_command
 from nextcord.ext import commands
 
 from lib.imdb import fetch
@@ -17,6 +17,10 @@ GENRES = set(
     genre.strip()
     for entry in DATA['Genres'].dropna()
     for genre in entry.split(',')
+)
+COUNTRIES = set(
+    country.strip()
+    for country in DATA['Country'].dropna()
 )
 
 
@@ -74,26 +78,97 @@ class IMDb(commands.Cog):
                 file=File(f, 'imdb.xlsx')
             )
 
-    # @slash_command(description='Generate a random movie')
+    @slash_command(description='Generate a random movie')
     async def pickmovie(self, interaction: Interaction,
         genre: str = SlashOption(
             description='Limit movies to a specific genre',
             required=False, autocomplete=True
+        ),
+        country: str = SlashOption(
+            description='Limit movies released in a specific country',
+            required=False, autocomplete=True
+        ),
+        year: int = SlashOption(
+            description='Limit movies to a specific year',
+            required=False
+        ),
+        year_min: int = SlashOption(
+            description='Limit movies released after or during the given year',
+            required=False, default=0
+        ),
+        year_max: int = SlashOption(
+            description='Limit movies released before or during the given year',
+            required=False, default=3000
+        ),
+        rating: int = SlashOption(
+            description='Limit movies to a specific rating',
+            required=False, choices=range(1, 11)
+        ),
+        rating_min: float = SlashOption(
+            description='Limit movies with a rating of at least the given rating',
+            required=False, default=1
+        ),
+        rating_max: float = SlashOption(
+            description='Limit movies with a rating of at most the given rating',
+            required=False, default=10
         )
     ) -> None:
-        interaction.response.defer()
+        await interaction.response.defer()
 
         df = DATA.copy()
         if genre:
-            df = df[df['Genres'].str.contains(f'\b{genre}\b', case=False, na=False)]
+            if genre not in GENRES:
+                await interaction.followup.send('Unrecognized genre: "{genre}"')
+                return
+            df = df[df['Genres'].str.contains(genre, na=False)]
+        if country:
+            if country not in COUNTRIES:
+                await interaction.followup.send('Unrecognized country: "{country}"')
+                return
+            df = df[df['Country'] == country]
+        if year:
+            df = df[df['Year'] == year]
+        if rating:
+            df = df[df['Rating'].between(rating, rating+0.9)]
 
-        pick = df.sample(n=1)
+        df = df[df['Year'].between(year_min, year_max)]
+        df = df[df['Rating'].between(rating_min, rating_max)]
 
-        resp = fetch(f'{API}/titles/{pick['tconst']}')
+        if df.empty:
+            await interaction.followup.send('No titles found with the given filter')
+            return
 
+        pick = df.sample(1)
 
+        resp = fetch(f'{API}/titles/{pick['tconst'].values[0]}')
 
-        await interaction.response.send_followup(pick)
+        if resp is None:
+            await interaction.followup.send('Fetch failed... Try again')
+            return
+
+        genres = [s['name'] for s in resp['interests'] if 'isSubgenre' not in s]
+        interests = [s['name'] for s in resp['interests'] if 'isSubgenre' in s]
+        if not genres:
+            genres = ['N/A']
+        if not interests:
+            interests = ['N/A']
+
+        embed = Embed(
+            title=resp['primaryTitle'],
+            url=f'https://www.imdb.com/title/{resp['id']}',
+            description=resp['plot']
+        )
+        embed.set_image(resp['primaryImage']['url'])
+        embed.add_field(name='Released', value=resp.get('startYear', 'N/A'))
+        embed.add_field(name='Runtime', value=timestr(resp.get('runtimeSeconds', 0))
+        embed.add_field(name='Rating', value=f'{pick['Rating']/10')
+        embed.add_field(name='Country',
+            value=f':flag_{resp['originCountries'][0]['code'].lower()}: '+pick['Country'].values[0]
+        )
+        embed.add_field(name='Genres', value=', '.join(genres))
+        embed.add_field(name='Interests', value=', '.join(interests))
+
+        await interaction.followup.send(embed=embed)
 
     @pickmovie.on_autocomplete('genre')
     async def genre_autocomplete(self, interaction: Interaction, curr: str):
@@ -104,6 +179,23 @@ class IMDb(commands.Cog):
 
         choices = dict(zip(matches, matches))
         await interaction.response.send_autocomplete(choices)
+
+    @pickmovie.on_autocomplete('country')
+    async def country_autocomplete(self, interaction: Interaction, curr: str):
+        if not curr:
+            matches = tuple(COUNTRIES)[:25]
+        else:
+            matches = tuple(c for c in COUNTRIES if curr.lower() in c.lower())
+
+        choices = dict(zip(matches, matches))
+        await interaction.response.send_autocomplete(choices)
+
+
+def timestr(sec: int) -> str:
+    secs = sec % 60
+    mins = (sec // 60) % 60
+    hours = sec // 3600
+    return f'{hours}:{mins:02}:{secs:02}'
 
 
 def setup(bot: commands.Bot):
