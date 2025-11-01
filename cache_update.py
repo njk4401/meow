563780686549710658bd9e03b08eea62d2ec9ccb
pipi <njk4401@rit.edu>
2025-11-01 08:17:13 -0400

    Update to use new IMDbCache

--- cache_update.py
+++ cache_update.py
@@ -8,36 +8,36 @@ from src.util import chunks, download_imdb_data
 
 print('\x1b[?25l', end='', flush=True)
 ids = set()
-with IMDbCache() as cache:
-    while True:
-        # Update IDs if needed
-        if any(download_imdb_data({
-            'title.basics.tsv.gz',
-            'title.ratings.tsv.gz'}
-            )) or not ids:
-            print('\r\x1b[0KReloading IDs...', end='', flush=True)
-            basics_list = []
-            sections = pd.read_csv('title.basics.tsv.gz', sep='\t',
-                                   na_values='\\N', compression='gzip',
-                                   usecols=['tconst', 'titleType'],
-                                   chunksize=50_000)
-            for chunk in sections:
-                chunk = chunk[chunk['titleType'] == 'movie']
-                basics_list.append(chunk)
-            basics = pd.concat(basics_list, ignore_index=True)
-            ratings = pd.read_csv('title.ratings.tsv.gz', sep='\t',
-                                  na_values='\\N', compression='gzip',
-                                  usecols=['tconst'])
-            df = basics.merge(ratings, on='tconst', how='inner')
-            df = df[df['titleType'].isin({'movie'})]
-            ids.update(df['tconst'])
+cache = IMDbCache()
+while True:
+    # Update IDs if needed
+    if any(download_imdb_data({
+        'title.basics.tsv.gz',
+        'title.ratings.tsv.gz'}
+        )) or not ids:
+        print('\r\x1b[0KReloading IDs...', end='', flush=True)
+        basics_list = []
+        sections = pd.read_csv('title.basics.tsv.gz', sep='\t',
+                               na_values='\\N', compression='gzip',
+                               usecols=['tconst', 'titleType'],
+                               chunksize=50_000)
+        for chunk in sections:
+            chunk = chunk[chunk['titleType'] == 'movie']
+            basics_list.append(chunk)
+        basics = pd.concat(basics_list, ignore_index=True)
+        ratings = pd.read_csv('title.ratings.tsv.gz', sep='\t',
+                              na_values='\\N', compression='gzip',
+                              usecols=['tconst'])
+        df = basics.merge(ratings, on='tconst', how='inner')
+        df = df[df['titleType'].isin({'movie'})]
+        ids.update(df['tconst'])
 
-        # Cache in batches of 5
-        for i, batch in enumerate(chunks(sorted(ids), 5)):
-            print(f'\r\x1b[0KCache Count: {cache.count()} ({((i*5)/len(ids))*100:.2f}%)', end='', flush=True)
-            try:
-                cache.add(*batch)
-            except Exception as e:
-                print(f'[{time.asctime()}] Failed to cache: {e}')
+    # Cache in batches of 5
+    for i, batch in enumerate(chunks(sorted(ids), 5)):
+        print(f'\r\x1b[0KCache Count: {await cache.count()} ({((i*5)/len(ids))*100:.2f}%)', end='', flush=True)
+        try:
+            await cache.add(*batch)
+        except Exception as e:
+            print(f'[{time.asctime()}] Failed to cache: {e}')
 
-        time.sleep(60)
+    time.sleep(60)
