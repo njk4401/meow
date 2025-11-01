8d59ef82184aa609f913229c2c51238340ef2b30
njk4401 <njk4401@rit.edu>
2025-11-01 07:49:11 -0400

    Remove leftover call

--- src/cmd/imdb.py
+++ src/cmd/imdb.py
@@ -22,7 +22,6 @@ class IMDbCog(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.bot.loop.create_task(self._load_autocompletions())
 
     #==========================================================================
     # Internal
